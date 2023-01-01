97def9cf517b3fd4b25f32af4536d492f0784f30
Stalmax76 <68605477+Stalmax76@users.noreply.github.com>
2023-01-01 11:49:06 +0100

    added function copy and watcher

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -1,28 +1,36 @@
 
 // отримуєм імя проєкту
+
 import * as nodePath from 'path';
 const rootFolder = nodePath.basename(nodePath.resolve());
 
-const buildFolder = `./dist`; // можливи використовувати імя проєкту
+const buildFolder = `./dist`; // можливо використовувати імя проєкту
 const srcFolder =`./src`;
 
 //об'єкт для збереження інформації шляху
+
 export const path={
 
-   // об'єкти 
-   //об'єкт шляхів готового результату
-   build:{
-      files:`${buildFolder}/files/`},
-   //об'єкт шляхів вихідних файлів
-   src:{
-files:`${srcFolder}/files/**/*.*`,
-   },
-   watch:{},// об'єкт шляхів спостереження
+                     // об'єкти 
+         //об'єкт шляхів готового результату
+      build:{
+            files:`${buildFolder}/files/`
+         },
 
-   //свойства
-   clean:buildFolder,    // clean така як результат роботи
-   buildFolder:buildFolder,// папка з результатом
-   srcFolder:srcFolder,    // папка з вихідними файлами
-   rootFolder:rootFolder,// ім'я поточного проєкту
-   ftp:``//шлях на віддалений сервер
+         //об'єкт шляхів вихідних файлів
+      src:{
+            files:`${srcFolder}/files/**/*.*`,
+         },
+         // об'єкт шляхів спостереження
+      watch:{
+            files:`${srcFolder}/files/**/*.*`,
+         },
+
+               //свойства
+
+      clean:buildFolder,    // clean така як результат роботи
+      buildFolder:buildFolder,// папка з результатом
+      srcFolder:srcFolder,    // папка з вихідними файлами
+      rootFolder:rootFolder,// ім'я поточного проєкту
+      ftp:``//шлях на віддалений сервер
 }
---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+        //головні модулі
+import gulp from "gulp";
+
+           //імпортуємо шляхи
+import {path} from "./gulp/config/path.js";
+
+// глобальна зміна для збереження основних даних
+global.app = {
+   path:path,
+   gulp:gulp
+}
+
+//імпортування завдань
+import{copy} from "./gulp/tasks/copy.js";
+
+//спостережувач за змінами в файлах
+function watcher(){
+   gulp.watch(path.watch.files, copy)
+}
+
+//побудова сценаріїв для виконання задач series() - виконує завдання послідовно
+const dev = gulp.series( copy, watcher);  //спочатку копіюєм а потім спостерігаєм
+
+//запуск виконання завдань за замовченням
+gulp.task('default', dev);
