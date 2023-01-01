ce8d3465f7e39f5ff42c220461f7ed9fae9c1cef
Stalmax76 <68605477+Stalmax76@users.noreply.github.com>
2023-01-01 17:06:36 +0100

    added plagin-version-number

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,8 +1,27 @@
 import fileinclude from "gulp-file-include";
+import webpHtmlNosvg from "gulp-webp-html-nosvg"; //обробка картинок
+import versionNumber from "gulp-version-number"; // плагін проти кешування
 
 export const html = () => {
    return app.gulp.src(app.path.src.html)
    .pipe(fileinclude())
    .pipe(app.plugins.replace(/@img\//g,'img/'))
+   .pipe(webpHtmlNosvg())
+   .pipe(
+      versionNumber({
+         'value': '%DT%',
+         'append':{
+            'key':'_v',
+            'cover': 0,
+            'to':[
+               'css',
+               'js',
+            ]
+         },
+         'output':{
+            'file': 'gulp/version.json'
+         }
+      })
+   )
    .pipe(app.gulp.dest(app.path.build.html))
 }
